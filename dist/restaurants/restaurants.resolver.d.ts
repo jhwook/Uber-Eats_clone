6a8ff7015453a0b4999f5d1dbd55cdb7f1735d83
jhwook <jhwooook@naver.com>
2022-10-06 23:00:16 +0900

    argument

---
+++ dist/restaurants/restaurants.resolver.d.ts
@@ -0,0 +1,4 @@
+import { Restaurant } from './entities/restaurant.entity';
+export declare class RestaurantResolver {
+    restaurants(veganOnly: boolean): Restaurant[];
+}
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,10 @@
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver((of) => Restaurant)
+export class RestaurantResolver {
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+}
